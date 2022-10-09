aeed37908735abe1a97bdc02c58e16a8f5be7bab
ukokobili <ukokobili.jacob@gmail.com>
2022-10-09 08:53:08 +0100

    homework

--- chp4/homework/predict.py
+++ chp4/homework/predict.py
@@ -1,28 +1,30 @@
-import pickle
-
 from flask import Flask
 from flask import request
 from flask import jsonify
 
+import pickle
 
-model_file = 'model_C=1.0.bin'
+def load(filename):
+    with open(filename, 'rb') as f_in:
+        return pickle.load(f_in)
 
-with open(model_file, 'rb') as f_in:
-    dv, model = pickle.load(f_in)
 
-app = Flask('churn')
+dv = load('dv.bin')
+model = load('model1.bin')
+
+app = Flask('card')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    customer = request.get_json()
+    client = request.get_json()
 
-    X = dv.transform([customer])
+    X = dv.transform([client])
     y_pred = model.predict_proba(X)[0, 1]
-    churn = y_pred >= 0.5
+    card = y_pred >= 0.5
 
     result = {
-        'churn_probability': float(y_pred),
-        'churn': bool(churn)
+        'card_probability': float(y_pred),
+        'card': bool(card)
     }
 
     return jsonify(result)
---
+++ chp4/homework/test.py
@@ -0,0 +1,10 @@
+from http import client
+import requests
+
+url = "http://localhost:9696/predict"
+
+client = {"reports": 0, "share": 0.245, "expenditure": 3.438, "owner": "yes"}
+respose = requests.post(url, json=client).json()
+
+print(respose)
+
